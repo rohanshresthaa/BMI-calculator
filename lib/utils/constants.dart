3e12cfed6129ab566a9b2a9f88930859efa75628
rohanshresthaa <r.shrestharohan@gmail.com>
2022-11-04 21:18:18 +0545

    added utilities

---
+++ lib/utils/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const activeCardColor = Color(0xFF404258);
+const bottomColor = Color(0xFFB9E0FF);
+const inActive = Color(0xFF6B728E);
+const labelText = TextStyle(
+  fontSize: 20.0,
+  color: Color(0xFFB9E0FF),
+);
+const numberText = TextStyle(fontSize: 50.0, fontWeight: FontWeight.w900);
