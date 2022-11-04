4d84cac761fc6fc38b378ad3c69d611d69b8a5e6
rohanshresthaa <r.shrestharohan@gmail.com>
2022-11-04 19:57:57 +0545

    refactor

--- lib/screens/widgets/reusableCard.dart
+++ lib/screens/widgets/reusableCard.dart
@@ -3,17 +3,21 @@ import 'package:flutter/cupertino.dart';
 class ReusableCard extends StatelessWidget {
   final Color rang;
   final Widget? cardChild;
-  ReusableCard({required this.rang, this.cardChild});
+  final Function()? onPress;
+  ReusableCard({required this.rang, this.cardChild, this.onPress});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(10.0),
-      decoration: BoxDecoration(
-        color: rang,
-        borderRadius: BorderRadius.circular(20.0),
+    return GestureDetector(
+      onTap: onPress,
+      child: Container(
+        margin: const EdgeInsets.all(10.0),
+        decoration: BoxDecoration(
+          color: rang,
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+        child: cardChild,
       ),
-      child: cardChild,
     );
   }
 }
