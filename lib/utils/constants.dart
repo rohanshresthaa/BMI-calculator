99d964814b2967f313fae853c08c98dbea555589
rohanshresthaa <r.shrestharohan@gmail.com>
2022-12-07 14:00:00 +0545

    color changes

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 const activeCardColor = Color(0xFF404258);
 const bottomColor = Color(0xFF404258);
-const inActive = Color(0xFF6B728E);
+const inActive = Color.fromARGB(255, 66, 96, 219);
 const labelText = TextStyle(
   fontSize: 20.0,
   color: Color(0xFFB9E0FF),
@@ -14,7 +14,7 @@ const bottomText = TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold);
 const resultText = TextStyle(fontSize: 50.0, fontWeight: FontWeight.bold);
 
 const resultStyle = TextStyle(
-    color: Color(0xFF24D876), fontSize: 22.0, fontWeight: FontWeight.bold);
+    color: Color.fromARGB(255, 16, 183, 91), fontSize: 22.0, fontWeight: FontWeight.bold);
 
 const bmiText = TextStyle(fontSize: 100.0, fontWeight: FontWeight.bold);
 
